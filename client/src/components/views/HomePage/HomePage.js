65712ec8278a934c3b7007d988a905af475b6438
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-09-15 20:06:10 +0900

    frontend - 홈페이지 제작중

--- client/src/components/views/HomePage/HomePage.js
+++ client/src/components/views/HomePage/HomePage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./HomePage.css";
+
+import JobOfferTable from "../Sections/JobOfferTable/JobOfferTable.js";
 
 function HomePage() {
-  return <div>홈페이지 입니다.</div>;
+  return (
+    <div id="home_page">
+      <section id="job_offer">
+        <h2>구인게시판</h2>
+        <JobOfferTable />
+      </section>
+
+      <section id="profile">
+        <h2>프로필</h2>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ client/src/components/views/Sections/JobOfferTable/JobOfferTable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JobOfferTable() {
+  return <div>구인게시판입니다.</div>;
+}
+
+export default JobOfferTable;
